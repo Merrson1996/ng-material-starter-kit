d97761976ec3b321a7784094dc71b1964d34f7f8
Merry <merrson1996@gmail.com>
2022-11-10 20:21:35 +0100

    Ćw 3 i 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,21 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
+import { HolidayListComponent } from './components/holiday-list/holiday-list.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
 import { CryptoServiceModule } from './services/crypto.service-module';
+import { HolidayListComponentModule } from './components/holiday-list/holiday-list.component-module';
+import { HolidayServiceModule } from './services/holiday.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent },
     { path: 'categories', component: CategoriesListComponent },
-    { path: 'crypto', component: CryptoListComponent }])
-    , ProductListComponentModule, ProductServiceModule, CategoriesListComponentModule, CategoriesServiceModule, CryptoListComponentModule, CryptoServiceModule],
+    { path: 'crypto', component: CryptoListComponent },
+    { path: 'public-holiday', component: HolidayListComponent }]), ProductListComponentModule, ProductServiceModule, CategoriesListComponentModule, CategoriesServiceModule, CryptoListComponentModule, CryptoServiceModule, HolidayListComponentModule, HolidayServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
