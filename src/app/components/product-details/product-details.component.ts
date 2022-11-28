84284d2146888f6dcb3e42c5324b23d509b2cbdd
Merry <merrson1996@gmail.com>
2022-11-29 00:18:09 +0100

    cw 3 detake

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,20 @@
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+import {ProductModel} from '../../models/product.model';
+import {ProductcwService} from '../../services/productcw.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductDetailsComponent {
+  readonly productDetails$: Observable<ProductModel> = this._activatedRoute.params
+    .pipe(switchMap(data => this._productcwService.getOne(data['id'])));
+
+  constructor(private _activatedRoute: ActivatedRoute, private _productcwService: ProductcwService) {
+  }
+}
---
+++ src/app/services/productcw.service-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProductcwService } from './productcw.service';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [ProductcwService],
+  exports: []
+})
+export class ProductcwServiceModule {
+}
